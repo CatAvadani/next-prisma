98c5510a481ea4e3635cbc4f7bb8d84d9c6ea5a8
CatAvadani <catalinaavadani@gmail.com>
2024-05-15 11:32:43 +0200

    Create validering with zod.

--- app/components/PostForm.tsx
+++ app/components/PostForm.tsx
@@ -1,10 +1,15 @@
 'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { savePost } from '../actions/actions';
-import { PostCreate } from '../validations/post';
+import { PostCreate, PostCreateSchema } from '../validations/post';
 
 export default function PostForm() {
-  const form = useForm<PostCreate>();
+  const form = useForm<PostCreate>({ resolver: zodResolver(PostCreateSchema) });
+
+  const {
+    formState: { errors },
+  } = form;
 
   const handleSubmit = async (data: PostCreate) => {
     await savePost(data);
@@ -21,11 +26,17 @@ export default function PostForm() {
         placeholder='Title'
         className='w-full p-2 border border-gray-300'
       />
+      {errors.title && (
+        <span className=' text-red-600'>{errors.title.message}</span>
+      )}
       <textarea
         {...form.register('content')}
         placeholder='Content'
         className='w-full p-2 border border-gray-300'
       ></textarea>
+      {errors.content && (
+        <span className=' text-red-600'>{errors.content.message}</span>
+      )}
       <button type='submit' className='w-full p-2 bg-blue-500 text-white'>
         Save Post
       </button>
--- app/validations/post.ts
+++ app/validations/post.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
-const PostCreateSchema = z.object({
+export const PostCreateSchema = z.object({
   title: z.string().min(3).max(50),
-  content: z.string().min(3),
+  content: z.string().min(10),
 });
 
 export type PostCreate = z.infer<typeof PostCreateSchema>;
